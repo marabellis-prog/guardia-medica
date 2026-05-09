0ea3d4dd0827f0ae94f81124bd4b97809028e390
marabellis-prog <marabelli.s@gmail.com>
2026-05-09 17:17:46 +0200

    deploy: script.js (build 1778339861744)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ─── BUILD VERSION (auto-aggiornato dallo script di deploy) ───
 // NON modificare manualmente: il deploy aggiorna questa stringa
-var BUILD_VERSION = '1778339781051';
+var BUILD_VERSION = '1778339861744';
 
 var POST=[],PAGE=1,PROW=null,DROW=null,DELEL=null;
 var dirtyMap={};
