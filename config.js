5a5851df4ad082fdc3e9eb969a8c8b4802af6497
marabellis-prog <marabelli.s@gmail.com>
2026-08-04 01:24:21 +0200

    deploy: config.js

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
 var SUPABASE_URL = 'https://lrvkchqvjzynfzevpqaj.supabase.co';
 var SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxydmtjaHF2anp5bmZ6ZXZwcWFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2NDY5NjksImV4cCI6MjA5MzIyMjk2OX0.XZXyUt9UNepHvr4HBLgCkywQYsXwtmvwYCsRrlAMBv4';
+
+// Google OAuth Client ID (pubblico) per l'upload allegati su Google Drive (scope drive.file)
+var GOOGLE_OAUTH_CLIENT_ID = '147428403592-i9lsbih9he2prlrl69j9on3qigj7gsm3.apps.googleusercontent.com';
